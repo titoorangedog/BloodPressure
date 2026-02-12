637c2abfad7398211a74c177fef299a2720a1a24
titoorangedog <ferrara.giuseppe@gmail.com>
2026-02-12 11:13:44 +0100

    Multilanguage + info

--- src/BloodPressure.Web/Program.cs
+++ src/BloodPressure.Web/Program.cs
@@ -9,6 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<TokenStorage>();
 builder.Services.AddScoped<JwtAuthenticationStateProvider>();
@@ -16,6 +17,7 @@ builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredServ
 builder.Services.AddTransient<AuthMessageHandler>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddSingleton<ToastService>();
+builder.Services.AddScoped<LocalizationService>();
 
 string ResolveApiBase(string key)
 {
